be21ec854a706bc1c458d7ce2fcf621e434a1b2d
leonardoz666 <royalez666@gmail.com>
2026-01-11 01:39:55 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,11 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
