3ad378679607d3cfd232ae8a33ca6c8c24de286b
leonardoz666 <royalez666@gmail.com>
2026-01-11 01:02:18 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -18,7 +18,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000'}/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_API_URL || 'https://backendrei.onrender.com'}/:path*`,
       },
     ]
   },
