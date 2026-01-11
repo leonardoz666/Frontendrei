a8477643501ae58c17932ee9286d523de5a9c216
leonardoz666 <royalez666@gmail.com>
2026-01-10 23:48:17 -0300

    att

--- app/cozinha/page.tsx
+++ app/cozinha/page.tsx
@@ -77,7 +77,7 @@ export default function KitchenPage() {
     run()
 
     // Socket.io connection
-    const socket = io('http://localhost:4000')
+    const socket = io(process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000')
 
     socket.on('connect', () => {
       console.log('Connected to socket')
